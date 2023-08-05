d6ca1bfdcdcbe0441960d9cce4664b8ed3db1015
tharindu senadheera <tharindu.senadeera@ascentic.se>
2023-08-05 06:15:33 +0530

    Initial Commit

---
+++ MovieMagicUITests/MovieMagicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieMagicUITestsLaunchTests.swift
+//  MovieMagicUITests
+//
+//  Created by Tharindu Senadheera on 8/5/23.
+//
+
+import XCTest
+
+final class MovieMagicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
